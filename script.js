93e32e29e960968e7b53e7fddbd27b999f329097
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-01-13 21:36:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var x = 1;
     x = x + 1;
    document.getElementById("votes").innerHTML= x +" SuPeR";
 if(x>10){
-document.getElementById("votes").innerHTML= x + "SuPeR" + 
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style=" color: green;"><br>Ai da-ma drecu, chiar asa de bun sunt ?</h5> ';
 }
      if(x>25){
-document.getElementById("votes").innerHTML= x + "SuPeR" + 
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style=" color: green;"><br>Un pahar de jin pentru asa apreciere!!</h5> ';
 }
 }
