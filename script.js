8165a4347e9289003d7d6284d1b5b61b3874938a
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-01-13 21:15:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var x = 1;
    function votes() { 
     x = x + 1;
    document.getElementById("votes").innerHTML= x +" SuPeR";
-     if(x>10){
-document.getElementById("votes").innerHTML= "Ai da-ma drecu, chiar asa de bun sunt ? ";
-       document.getElementById("votess").innerHTML= "URAA";
+if(x>15){
+document.getElementById("votes").innerHTML= x + "SuPeR" + 
+'<h5 style=" color: green;"><br>Ai da-ma drecu, chiar asa de bun sunt ?</h5> ';
+}
+}
+function vvotes() {
+x = x - 1;
+document.getElementById("votes").innerHTML= x +" SuPeR";
+if(x<-1){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Si ai ahuit blea? ?</h5> ';
 }
    }
