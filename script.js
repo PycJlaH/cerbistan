e070693211986c951b3c73f3c09b0095f7e2542f
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-01-13 20:38:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ var x = 1;
    function votes() { 
     x = x + 1;
    document.getElementById("votes").innerHTML= x +" SuPeR";
+     if(x>30){
+document.getElementById("votes").innerHTML= "  Ai da-ma drecu, chiar asa de bun sunt ? ";
+}
    }
