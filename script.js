35734db34eac7fb88495da4a0e59b2046f9d7cf7
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-01-13 20:47:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var x = 1;
    function votes() { 
     x = x + 1;
    document.getElementById("votes").innerHTML= x +" SuPeR";
-     if(x>30){
+     if(x>10){
 document.getElementById("votes").innerHTML= "Ai da-ma drecu, chiar asa de bun sunt ? ";
        document.getElementById("votess").innerHTML= "URAA";
 }
