97428dc93d5811a6f330189a43a720367b6ef1b3
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-01-22 19:44:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,56 @@ var x = 1;
    function votes() { 
     x = x + 1;
    document.getElementById("votes").innerHTML= x +" SuPeR";
-if(x>10){
+if(x>5){
 document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style=" color: green;"><br>Ai da-ma drecu, chiar asa de bun sunt ?</h5> ';
 }
-     if(x>25){
+     if(x>10){
 document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style=" color: green;"><br>Un pahar de jin pentru asa apreciere!!</h5> ';
+}
+     if(x>20){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style=" color: green;"><br>Good stuff!!</h5> ';
+}
+     if(x>50){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style=" color: green;"><br>Uvajuha!!</h5> ';
 }
 }
 function vvotes() {
 x = x - 1;
 document.getElementById("votes").innerHTML= x +" SuPeR";
-if(x<-1){
+if(x<0){
 document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style="color: green;"><br> Si ai ahuit blea? ?</h5> ';
 }
-  if(x<-10){
+  if(x<-5){
 document.getElementById("votes").innerHTML= x + " SuPeR" + 
 '<h5 style="color: green;"><br> Ai belit pula !!</h5> ';
+   }
+  if(x<-10){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Te fut in cur!!</h5> ';
+   }
+  if(x<-15){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Tu la halloween nu porti masca!!</h5> ';
+   }
+  if(x<-20){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Chizda / Chizdon</h5> ';
+   }
+  if(x<-50){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Sa ma sugi la coaie!!</h5> ';
+   }
+  if(x<-75){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Unde te-am prins acolo te-am zaibit!</h5> ';
+   }
+  if(x<-100){
+document.getElementById("votes").innerHTML= x + " SuPeR" + 
+'<h5 style="color: green;"><br> Ajunge blea, variante nu mai am!</h5> ';
    }
 }
