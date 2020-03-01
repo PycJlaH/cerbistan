64d95345ba81d09e92be97001a9e58ef2ef8efa5
PycJlaH <59747509+PycJlaH@users.noreply.github.com>
2020-03-01 12:36:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -279,4 +279,10 @@ if(x==b){
 }
 window.alert(x + c + v + n + 'cu ' + b);
 }
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
